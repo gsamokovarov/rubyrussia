f2117b3c7157bc9e5a3038d7e9282ae44816ad7f
Genadi Samokovarov <gsamokovarov@gmail.com>
2019-09-13 17:19:39 +0300

    Add an extra space after the сърпичук

--- config/initializers/rvt.rb
+++ config/initializers/rvt.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 if defined?(RVT)
-  Rails.configuration.rvt.command = "BASH_SILENCE_DEPRECATION_WARNING=1 PS1='(`basename $PWD`) ☭ ' /bin/sh"
+  Rails.configuration.rvt.command = "BASH_SILENCE_DEPRECATION_WARNING=1 PS1='(`basename $PWD`) ☭  ' /bin/sh"
   Rails.configuration.rvt.style.font_size = 24
 end
